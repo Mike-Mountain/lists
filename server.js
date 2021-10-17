9a076fc35abd60dd8296fa05c794694ccc9e4f15
Michael <mikemountain.dev@gmail.com>
2021-10-17 18:13:09 +0200

    Config updates

--- server.js
+++ server.js
@@ -22,12 +22,12 @@ app.use(forceSSL());
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/lists/index.html"));
 });
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/lists"));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
