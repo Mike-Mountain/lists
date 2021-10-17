eb694bc6d4211853ca9f3c6537089023537e2567
Michael <mikemountain.dev@gmail.com>
2021-10-17 14:04:19 +0200

    Create heroku environment

---
+++ src/environments/environment.heroku.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://honeystone-mountain.herokuapp.com',
+  unsplashApi: 'https://api.unsplash.com/',
+  accessKey: process.env.UNSPLASH_SECRET,
+};
